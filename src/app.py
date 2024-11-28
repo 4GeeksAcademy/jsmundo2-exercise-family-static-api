2e2abf51c9871c2c4f645ab1aa8c9db4d3e36fc5
Alexader Quejada cordoba <quejadacordobaalexander@gmail.com>
2024-11-28 12:04:43 +0000

    familia Jackson

--- src/app.py
+++ src/app.py
@@ -26,17 +26,63 @@ def sitemap():
     return generate_sitemap(app)
 
 @app.route('/members', methods=['GET'])
-def handle_hello():
+def get_all_members():
+    members = jackson_family.get_all_members()
+    if members == []:
+        return jsonify({"error": "there is not member"}), 404
+    return jsonify(members),200
+    
+    
 
     # this is how you can use the Family datastructure by calling its methods
-    members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
+@app.route('/member/<int:member_id>', methods=['GET']) 
+def get_member(member_id):
+    member = jackson_family.get_member(member_id)
+    if member:
+        return jsonify(member), 200
+    return jsonify({"error": "Member not found"}),400
 
 
-    return jsonify(response_body), 200
+
+@app.route('/member', methods=['POST'])
+def add_member():
+    body = request.json
+    if not body or "first_name" not in body or "age" not in body or "lucky_numbers" not in body:
+        return jsonify({"error": "Missing required filds"}), 400
+    
+    
+    added_member = jackson_family.add_member(body)
+    return (added_member),200
+
+
+@app.route('/member/<int:member_id>', methods=['DELETE'])
+def delete_member(member_id):
+    success = jackson_family.delete_member(member_id)
+    if success:
+        return jsonify({"message": "Member deleted successfully","done":True}),200
+    return jsonify({"error": "Member not found"}), 400
+
+
+@app.route('/members/<int:member_id>', methods=['PUT'])
+def update_member(member_id):
+    body = request.get_json()
+    if not body:
+        return jsonify({"error": "Missing request body"}), 400
+    
+    update_member = jackson_family.update_member(member_id, body)
+    if update_member:
+        return jsonify(update_member), 200
+    return jsonify({"error": "Member not found"}), 400
+
+
+
+
+
+       
+        
+
+
+    
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
